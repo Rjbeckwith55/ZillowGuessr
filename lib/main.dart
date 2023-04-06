9d59b1c0d6729f75068954b028b02b86329a7cb2
rjbeckwith55 <rjbeckwith55>
2023-04-06 00:49:47 -0500

    add an image slider

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
 import 'friends_list.dart';
+import 'package:flutter_image_slideshow/flutter_image_slideshow.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
 
 void main() => runApp(MyApp());
 
@@ -63,7 +65,7 @@ class InvitePage extends StatelessWidget {
               onPressed: () {
                 Navigator.push(
                   context,
-                  MaterialPageRoute(builder: (context) => Loading()),
+                  MaterialPageRoute(builder: (context) => GameScreen()),
                 );
               },
             ),
@@ -87,7 +89,6 @@ class InvitePage extends StatelessWidget {
   }
 }
 
-
 class Loading extends StatelessWidget {
   const Loading();
 
@@ -95,56 +96,95 @@ class Loading extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Invite Friends'),
+        title: const Text('Loading'),
       ),
       body: Column(
-          mainAxisAlignment: MainAxisAlignment.center, // centers vertically
-          crossAxisAlignment:
-          CrossAxisAlignment.stretch, // centers horizontally
-          children: [Text('Waiting for Players!')],
-    ),
+        mainAxisAlignment: MainAxisAlignment.center, // centers vertically
+        crossAxisAlignment: CrossAxisAlignment.stretch, // centers horizontally
+        children: [Text('Waiting for Players!')],
+      ),
     );
   }
 }
 
-class RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
-  final _biggerFont = const TextStyle(fontSize: 18.0);
+class GameScreen extends StatelessWidget {
+  const GameScreen();
+
   @override
   Widget build(BuildContext context) {
+    var lat = "lat";
+    var long = "long";
+    var baths = "1";
+    var beds = "3";
+    var squareFeet = 1500;
+    var year = "1955";
     return Scaffold(
       appBar: AppBar(
-        title: Text('Zillow Guesser'),
+        title: const Text('Guess the price'),
       ),
-      body: _buildSuggestions(),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center, // centers vertically
+        crossAxisAlignment: CrossAxisAlignment.center, // centers horizontally
+        children: [
+          Row(
+              mainAxisAlignment: MainAxisAlignment.center, // centers vertically
+              crossAxisAlignment:
+                  CrossAxisAlignment.center, // centers horizontally
+              children: [
+                ImageSlideshow(
+                  /// Width of the [ImageSlideshow].
+                  width: 600,
+
+                  /// Height of the [ImageSlideshow].
+                  height: 600,
+
+                  /// The page to show when first creating the [ImageSlideshow].
+                  initialPage: 0,
+
+                  /// The color to paint the indicator.
+                  indicatorColor: Colors.blue,
+
+                  /// The color to paint behind th indicator.
+                  indicatorBackgroundColor: Colors.grey,
+
+                  children: [
+                    Image.network(
+                      'https://photos.zillowstatic.com/fp/e664f0c3c87d4a2d344146d5c1adf8fc-p_e.jpg',
+                      fit: BoxFit.cover,
+                    ),
+                    Image.network(
+                      'https://photos.zillowstatic.com/fp/b6daf9d5b6d48101bc27719b07b66c6d-p_e.jpg',
+                      fit: BoxFit.cover,
+                    ),
+                    Image.network(
+                      'https://photos.zillowstatic.com/fp/00669b1b504cd204b56873444ffe8255-p_e.jpg',
+                      fit: BoxFit.cover,
+                    ),
+                  ],
+
+                  /// Called whenever the page in the center of the viewport changes.
+                  onPageChanged: (value) {
+                    print('Page changed: $value');
+                  },
+
+                  isLoop: true,
+                ),
+              ]),
+          Row(
+              mainAxisAlignment: MainAxisAlignment.center, // centers vertically
+              crossAxisAlignment:
+                  CrossAxisAlignment.center, // centers horizontally
+              children: [Text("Baths: $baths "), Text("Beds: $beds")]),
+          Row(
+              mainAxisAlignment: MainAxisAlignment.center, // centers vertically
+              crossAxisAlignment:
+                  CrossAxisAlignment.center, // centers horizontally
+              children: [
+                Text("Square Ft: $squareFeet "),
+                Text("Year Built: $year")
+              ]),
+        ],
+      ),
     );
   }
-
-  Widget _buildSuggestions() {
-    return ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        itemBuilder: /*1*/ (context, i) {
-          if (i.isOdd) return Divider(); /*2*/
-
-          final index = i ~/ 2; /*3*/
-          if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(10)); /*4*/
-          }
-          return _buildRow(_suggestions[index]);
-        });
-  }
-
-  Widget _buildRow(WordPair pair) {
-    return ListTile(
-      title: Text(
-        pair.asPascalCase,
-        style: _biggerFont,
-      ),
-    );
-  }
-}
-
-class RandomWords extends StatefulWidget {
-  @override
-  RandomWordsState createState() => new RandomWordsState();
 }
