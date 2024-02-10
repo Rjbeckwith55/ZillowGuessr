40c84eb6b2950d005435a2758e3160c55c6c6cd9
rjbeckwith55 <rjbeckwith55>
2024-02-09 23:46:29 -0600

    update test import

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:hello_world/main.dart';
+import 'package:zillow_guesser/main.dart';
 
 void main() {
   testWidgets('Do nothing', (WidgetTester tester) async {
